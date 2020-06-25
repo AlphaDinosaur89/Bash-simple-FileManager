f35cf52af62fff038616589be8b01ef1bc096278
AlphaDinosaur89 <51428267+AlphaDinosaur89@users.noreply.github.com>
2020-06-25 14:01:39 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+i=0
+
+clear
+echo ""
+echo "Simple FileManger installer"
+echo ""
+sleep 2
+
+sudo rm -r -f /usr/bin/Manager
+
+sudo cp Manager /usr/bin/Manager
+
+clear
+echo "Installation process finished"
+sleep 1
